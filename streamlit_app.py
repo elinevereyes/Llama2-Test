12953cd95ea577aa6fd8d268d9126bbcbead3b0d
elinevereyes <110269546+elinevereyes@users.noreply.github.com>
2024-04-15 16:36:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ with st.sidebar:
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon=⚠️')
+            st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
 
